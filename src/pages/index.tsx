8555024a8e99841bccaa7d867ddc67f6dd862d81
Caleb Taylor <aquaductape@gmail.com>
2021-12-01 12:14:34 -0800

    update

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,22 +1,31 @@
 import Counter from "~/components/Counter";
 import "./index.css";
-import Dismiss from "solid-dismiss";
 import { createSignal } from "solid-js";
+import CreatePortal from "~/components/CreatePortal";
+import { isServer } from "solid-js/web";
 
 export default function Home() {
   const [open, setOpen] = createSignal(false);
   let menuButton!: HTMLButtonElement;
+
+  if (open && !isServer) {
+    CreatePortal({
+      mount: document.body,
+      popupChildren: <div>hi</div>,
+      overlayChildren: null,
+      marker: null,
+    });
+  }
+
   return (
     <main>
       <h1>Hello world!</h1>
       <Counter />
       <p>
         Visit <button ref={menuButton}>hi</button>
-        <Dismiss menuButton={menuButton} open={open} setOpen={setOpen}>
-          <a href="https://solidjs.com" target="_blank">
-            solidjs.com
-          </a>{" "}
-        </Dismiss>
+        <a href="https://solidjs.com" target="_blank">
+          solidjs.com
+        </a>{" "}
         to learn how to build Solid apps.
       </p>
     </main>
